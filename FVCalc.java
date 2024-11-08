cc3d4cd887f1b43430fb768c29356d042328404f
NoaOz <noa.oz01@post.runi.ac.il>
2024-11-08 20:44:40 +0200

    Completed 0.4 FVCalc

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,8 @@ public class FVCalc {
 		int currentValue = Integer.parseInt(args[0]);
 		double rate = Double.parseDouble(args[1]);
 		int n = Integer.parseInt(args[2]);
-		double futureValue = currentValue * Math.pow((1 + (rate/100.0)), (double)n);
-		System.out.println("After " + n + " years, $" + currentValue + " saved at " + rate + "% " + "will yield $" + (int) futureValue);
+		double futureValue = currentValue * Math.pow((1 + (rate/100.0)), n);
+		System.out.println("After " + n + " years, $" + currentValue + " saved at " + rate + "%25 " + "will yield $" + (int) futureValue);
 
 	}
 }
